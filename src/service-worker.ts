33d1df857cd09bf31e2ab5cf2b451edde4e979d0
mustofa-id <25121822+mustofa-id@users.noreply.github.com>
2025-04-04 08:57:39 +0700

    feat: notification actions

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -5,19 +5,35 @@
 
 const sw = self as unknown as ServiceWorkerGlobalScope;
 
+// experimental: afaik only supported by chrome based browser
+// see: https://developer.mozilla.org/en-US/docs/Web/API/ServiceWorkerRegistration/showNotification#actions
+interface NotificationAction {
+	action: string;
+	title: string;
+	icon?: string;
+}
+
 sw.addEventListener('push', (e) => {
 	const notification = e.data?.json() || {};
-	const options: NotificationOptions = {
+	const options: NotificationOptions & { actions: NotificationAction[] } = {
 		icon: '/favicon.png',
 		badge: '/favicon.png',
 		body: notification.body || '',
-		data: notification.data || {}
+		data: notification.data || {},
+		requireInteraction: true,
+		actions: [
+			{ action: 'open', title: 'Open' },
+			{ action: 'dismiss', title: 'Dismiss' }
+		]
 	};
 	e.waitUntil(sw.registration.showNotification(notification.title, options));
 });
 
 sw.addEventListener('notificationclick', (e) => {
 	e.notification.close();
-	const targetUrl = e.notification.data.url;
-	if (targetUrl) e.waitUntil(sw.clients.openWindow(targetUrl));
+
+	if (e.action == 'open') {
+		const targetUrl = e.notification.data.url;
+		if (targetUrl) e.waitUntil(sw.clients.openWindow(targetUrl));
+	}
 });
